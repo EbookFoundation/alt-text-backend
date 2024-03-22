8295db55837612bc6042a7fd7ced5c8a4d7b4e62
xxmistacruzxx <dacruz04@optonline.net>
2024-03-22 14:15:07 -0400

    Added versioning to requirements.txt.

--- alttextbackend/settings.py
+++ alttextbackend/settings.py
@@ -77,13 +77,13 @@ WSGI_APPLICATION = "alttextbackend.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ['DATABASE_NAME'],
-        'USER': os.environ['DATABASE_USER'],
-        'PASSWORD': os.environ['DATABASE_PASSWORD'],
-        'HOST': os.environ['DATABASE_HOST'], # Or the address of your database server
-        'PORT': os.environ['DATABASE_PORT'], # Default PostgreSQL port
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ["DATABASE_NAME"],
+        "USER": os.environ["DATABASE_USER"],
+        "PASSWORD": os.environ["DATABASE_PASSWORD"],
+        "HOST": os.environ["DATABASE_HOST"],  # Or the address of your database server
+        "PORT": os.environ["DATABASE_PORT"],  # Default PostgreSQL port
     }
 }
 
